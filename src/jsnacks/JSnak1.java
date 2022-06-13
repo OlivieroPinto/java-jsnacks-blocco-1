2f776b75a007969bf772f56c288161068fe39353
OlivieroPinto <pintooliviero@gmail.com>
2022-06-13 10:30:33 +0200

    push jsnack1

---
+++ src/jsnacks/JSnak1.java
@@ -0,0 +1,22 @@
+package jsnacks;
+
+import java.util.Scanner;
+
+public class JSnak1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int numero = 0;
+		int sommanumeri = 0;
+		System.out.println("scrivi 5 numeri e io ti diró la loro somma");
+		for(int i=1; i<=5; i++) {
+			numero = scan.nextInt();
+			sommanumeri = sommanumeri + numero;
+			
+
+		}
+		System.out.println(sommanumeri);
+
+	}
+
+}
