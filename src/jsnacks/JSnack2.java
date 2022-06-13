45317cfae543a0e4cbb5182902066594f1d578e2
OlivieroPinto <pintooliviero@gmail.com>
2022-06-13 10:35:45 +0200

    push jsnak2

---
+++ src/jsnacks/JSnack2.java
@@ -0,0 +1,21 @@
+package jsnacks;
+
+import java.util.Scanner;
+
+public class JSnack2 {
+
+	public static void main(String[] args) {
+		int numero;
+		System.out.println("inserisci un nuemro:");
+		Scanner scan = new Scanner(System.in);
+		numero = scan.nextInt();
+		if (numero % 2 == 0) {
+			System.out.println(numero);
+		} else {
+			numero++;
+			System.out.println(numero);
+		}
+
+	}
+
+}
